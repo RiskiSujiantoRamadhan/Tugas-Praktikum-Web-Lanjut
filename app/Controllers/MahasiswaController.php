3311f17a2e8788fc1e0f76791ccdd0dfc30398ad
Riski Sujianto Ramadhan <anarchyricky474@gmail.com>
2022-09-21 05:38:54 +0700

    Tugas 4

--- app/Controllers/MahasiswaController.php
+++ app/Controllers/MahasiswaController.php
@@ -4,46 +4,53 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\Mahasiswa;
+
 class MahasiswaController extends BaseController
 {
     public function index()
     {
         $mahasiswaModel = new Mahasiswa();
         $mahasiswa = $mahasiswaModel->findAll();
+
         $data = [
             'title' => 'Mahasiswa',
             'mahasiswa' => $mahasiswa
-        ]; 
+        ];
         return view('templates/header', $data)
-        . view('mahasiswa/list', $data)
-        . view('templates/footer');
+            . view('mahasiswa/list' , $data)
+            . view('templates/footer');  
     }
+
     public function create(){
         $data = [
             'title' => 'Create Mahasiswa'
         ];
+        
         return view('templates/header', $data)
-        . view('mahasiswa/create')
-        . view('templates/footer');
+            . view('mahasiswa/create')
+            . view('templates/footer');
     }
-    public function store()
-    {
+
+    public function store(){
         if(!$this->validate([
             'npm' => 'required',
             'nama' => 'required',
-            'alamat' => 'required',
+            'alamat' => 'required'
         ])){
             return redirect()->to('/create');
         }
+
         $mahasiswaModel = new Mahasiswa();
         $data = [
             'npm' => $this->request->getPost('npm'),
             'nama' => $this->request->getPost('nama'),
-            'alamat' => $this->request->getPost('alamat'),
+            'alamat' => $this->request->getPost('alamat')
         ];
+
         $mahasiswaModel->save($data);
         return redirect()->to('/mahasiswa');
     }
+
     public function delete($id)
     {
         $mahasiswaModel = new Mahasiswa();
@@ -51,33 +58,36 @@ class MahasiswaController extends BaseController
 
         return redirect()->to('/mahasiswa');
     }
-    public function edit($id) {
+
+    public function edit($id){
         $mahasiswaModel = new Mahasiswa();
-        $mahasiswaModel->find($id);
+        $mahasiswa = $mahasiswaModel->find($id);
 
         $data = [
             'title' => 'Edit Mahasiswa'
         ];
 
         return view('templates/header', $data)
-        . view('mahasiswa/edit', $mahasiswa)
-        . view('templates/footer');
+            . view('mahasiswa/edit', $mahasiswa)
+            . view('templates/footer');
     }
-    public function update($id)
-    {
+
+    public function update($id){
         if(!$this->validate([
             'npm' => 'required',
             'nama' => 'required',
-            'alamat' => 'required',
+            'alamat' => 'required'
         ])){
-            return redirect()->to('/edit/'.$id);
+            return redirect()->to('/edit/' .$id);
         }
+
         $mahasiswaModel = new Mahasiswa();
         $data = [
             'npm' => $this->request->getVar('npm'),
             'nama' => $this->request->getVar('nama'),
-            'alamat' => $this->request->getVar('alamat'),
+            'alamat' => $this->request->getVar('alamat')
         ];
+
         $mahasiswaModel->update($id, $data);
         return redirect()->to('/mahasiswa');
     }
